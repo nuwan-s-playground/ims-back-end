8326b44ca76fc0874b2cba946fd73a79f9e9dc9d
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-11-23 00:06:38 +0530

    complete course backend

--- src/main/java/lk/ijse/dep11/ims/to/CourseTO.java
+++ src/main/java/lk/ijse/dep11/ims/to/CourseTO.java
@@ -5,13 +5,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.groups.Default;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CourseTO implements Serializable {
-    private int courseId;
+    @Null(message = "Id should be empty", groups = Create.class)
+    //@NotNull(message = "should not be null", groups = Update.class)
+    private Integer courseId;
+
+    @NotBlank(message = "name should not be empty")
     private String name;
-    private int duration_in_months;
+
+    //@Null(message = "Status should be empty", groups = Update.class)
+    @NotNull(message = "duration should not be empty")
+    private Integer duration_in_months;
+
+    public interface Update extends Default {}
+    public interface Create extends Default{}
 }
