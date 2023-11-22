5467498571ee4cc6a9f3b47226b471c99d1b43a4
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-11-22 17:33:58 +0530

    initiate course related web services

--- src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
+++ src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
@@ -1,12 +1,29 @@
 package lk.ijse.dep11.ims.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/courses")
 public class CourseHttpController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public void createCourse(){}
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/${courseID}", consumes = "application/json")
+    public void updateCourse(){}
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/${courseID}")
+    public void deleteCourse(){}
+
+    @GetMapping(value = "/${courseID}", produces = "application/json")
+    public void getCourseDetails(){}
+
+    @GetMapping(value = "/${courseID}", produces = "application/json")
+    public void getAllCourses(){}
 
 }
